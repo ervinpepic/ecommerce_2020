a9afa85e7e501b9b6b60ee701fbd922002b0bc0f
Ervin Pepic <ervin.hack@gmail.com>
2020-08-20 20:27:56 +0200

    Added some dynamic context dictionary data type variables and some test data

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -3,4 +3,22 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home_page.html', {})
+    context = {
+        "title": "Hello World!",
+        "content": "Welcome to the home page."
+    }
+    return render(request, 'home_page.html', context)
+
+def about_page(request):
+    context = {
+        "title": "About Page!",
+        "content": "Welcome to the about page."
+    }
+    return render(request, 'home_page.html', context)
+
+def contact_page(request):
+    context = {
+        "title": "Contact Page!",
+        "content": "Welcome to the contact page."
+    }
+    return render(request, 'home_page.html', context)
